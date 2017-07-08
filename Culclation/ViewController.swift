b7ed856dc08fd99cff1b11ce0dcec05784b2bcb1
ryonakao <rodriguez.nak@gmail.com>
2017-07-08 14:08:09 +0900

    complete input number

--- Culclation/ViewController.swift
+++ Culclation/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var priceField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tab1Button(_ sender: Any) {
+        let value = priceField.text! + "1"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+        priceField.text = "0"
+    }
+    
 
 }
-
